b29edc29ae99705a9f276602a84fbcc11a9e3dee
Elisabet <elisabetaudiovisual@gmail.com>
2023-04-02 00:44:22 +0200

    Nombre Refactorizado

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Test;
 public class TestCoche {
     @Test
 
-    public void test_crear_coche_su_velocidad_es_cero() {
+    public void test_crear_coche_su_velocidad_es_cero_Elisabet_Agullo() {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0,nuevoCoche.velocidad);
     }
     @Test
-    public void test_acelerar_un_coche_su_velocidad_aumenta() {
+    public void test_acelerar_un_coche_su_velocidad_aumenta_Elisabet_Agullo() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.acelerar(30);
       Assertions.assertEquals(30,nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_decelerar_un_coche_su_velocidad_disminuye() {
+    public void test_decelerar_un_coche_su_velocidad_disminuye_Elisabet_Agullo() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
         nuevoCoche.decelerar(20);
@@ -24,7 +24,7 @@ public class TestCoche {
     }
 
     @Test
-    public void test_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero() {
+    public void test_decelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero_Elisabet_Agullo() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
         nuevoCoche.decelerar(80);
